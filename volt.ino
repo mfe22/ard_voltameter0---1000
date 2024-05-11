4c795b2a1842ffd3841d04d5e245604b5b263e1c
mohamed fawzy <151654898+mfe22@users.noreply.github.com>
2024-05-11 13:12:37 +0300

    Update volt.ino
    ohmmeter circuit added

--- volt.ino
+++ volt.ino
@@ -1,52 +1,122 @@
 #include <LiquidCrystal.h>
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 9, en = 8, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7); 
 void setup() {
 lcd.begin(16, 2);
 
 pinMode(A0 , INPUT) ;
-pinMode(A1 , INPUT) ;
 pinMode(A2 , INPUT) ;
-pinMode(0,INPUT);
-pinMode(7,OUTPUT);
+pinMode(15, INPUT);
+pinMode(6, INPUT);
+pinMode(16 , INPUT) ;
 
 }
-
-
+int x = 0 ;
 void loop() {
-
-  float s1 = analogRead(A0);
-  float s2 = analogRead(A1);
-  float s3 = analogRead(A2); 
-  if(digitalRead(0) == 1){
-  digitalWrite(7, HIGH);
+  if (digitalRead(16) == HIGH && x == 0 ){
+    x = 1 ;
+ delay(500);
   }
-  else{
-    digitalWrite(7,LOW);
+  else if (digitalRead(16) == HIGH && x == 1 ){
+    x = 0 ;
+    delay(500);
   }
-  if(s1 < 10 && abs(s2)<20.5 && abs(s3) <20.5){
+  float V = analogRead(A0);
+  float RV = (analogRead(A2)); 
+  int selectR = digitalRead(15);
+  int selectV = digitalRead(6);
+if( x == 0) {
+  lcd.clear();
+  //voltmeter code
   lcd.setCursor(0,0);
-  lcd.print("voltmeter         ");
-  lcd.setCursor(1,1);
-  lcd.print("                           ");
+  lcd.print("V=");
+  if(selectV == HIGH){
+      if( V < 518 && V >500){
+    lcd.setCursor(2,0);
+  lcd.print("0" );
+    lcd.setCursor(5,1);
+  lcd.print("-10 to 10");
+   delay(555);
+  }
+    else{
+  lcd.setCursor(2,0);
+  lcd.print(((V-508.85)*3.96*5.03/1023) ,2);
+    lcd.setCursor(5,1);
+  lcd.print("-10 to 10");
+   delay(555);
+      }
+  }
+  if(selectV == LOW){
+    if(V < 527.78 && V >517.57){
+  lcd.setCursor(2,0);
+    lcd.print("0");
+        lcd.setCursor(5,1);
+    lcd.print("-50 to 50");
+   delay(555);
+    }
+    else{
+  lcd.setCursor(2,0);
+    lcd.print(((V-522.68)*19.91908*5.03/1023),2);
+        lcd.setCursor(5,1);
+    lcd.print("-50 to 50");
+   delay(555);
+    }}
 }
-if(s1 > 0 && abs(s2)<20.5 && abs(s3) <20.5){
+else{
+  //OHMMETER FIRST SCALE
+  lcd.clear();
+ if(selectR){
+  lcd.setCursor(6,1);
+  lcd.print("300 to 10k");
+  if(RV>90 && RV<995){
+  lcd.setCursor(3, 0);
+  lcd.print((49250-(48.095750313*(RV)))/((RV)*5.03/1023));
   lcd.setCursor(0,0);
-  lcd.print("volt =      ");
-  lcd.setCursor(1 , 1 );
-  lcd.print((s1*10/1024)+.05);
-}
-if(s2> 0 && abs(s1)<20.5 && abs(s3) <20.5){
+  lcd.print(" R=");
+  delay(150);
+  }
+  else if(RV>=995) {
+   lcd.setCursor(3, 0);
+  lcd.print("0");
   lcd.setCursor(0,0);
-  lcd.print("volt =          ");
-  lcd.setCursor(1 , 1 );
-  lcd.print(s2*20*5/1024);
-}
-if(s3 > 0 && abs(s2)<20.5 && abs(s1) <20.5){
-  lcd.setCursor(0,0);
-  lcd.print("volt =          ");
-  lcd.setCursor(1 , 1 );
-  lcd.print(s3*200*5/1024);
-}
+  lcd.print(" R=");
+  delay(500);
+  }
+  else {
 
+  lcd.setCursor(3, 0);
+  lcd.print("o.l");
+  lcd.setCursor(0,0);
+  lcd.print(" R=");
+  delay(500);
+  }
+ }
+ else if(!selectR){
+  //OHMMETER SECOND SCALE
+    lcd.clear();
+  lcd.setCursor(7,1);
+  lcd.print("10k to 1M");
+ if(RV>92 && RV<935){
+  lcd.setCursor(3, 0);
+  lcd.print(((499000-(487.3046875*RV))/(RV*5.03/1023))/1000);
+  lcd.setCursor(0,0);
+  lcd.print(" R=");
+  lcd.setCursor(15,0);
+  lcd.print("k");
+  delay(500); }
+  else if(RV>=935) {
+   lcd.setCursor(3, 0);
+  lcd.print("0");
+  lcd.setCursor(0,0);
+  lcd.print(" R=");
+  delay(500); }
+  else {
+  lcd.setCursor(3, 0);
+  lcd.print("o.l");
+  lcd.setCursor(0,0);
+  lcd.print(" R=");
+  delay(500); }
+  }
+}
+delay(300);
 }
